c1158b2f40a78674514073ed05489d8de92eb88f
unknown <kopranych@gmail.com>
2017-06-01 14:40:41 +0400

    timet2_3 is work

--- Timer.c
+++ Timer.c
@@ -1,23 +1,27 @@
 //Function timer
 
+
+
 #include "Timer.h"
 
 
 
 void init_timer1(void)
 {
+    /////// ???????????!!! ????????? ? CONFIG3 SOSCSEL_SOSC = External clock (SCLKI) or Digital I/) mode
+    ////// ??? ????? TIMER1 ????? ???????? ?? ??????? ?????? ?????? ?? ?????? ??????? ?? 23???!!!!
     _T1IF = 0;//clear flag interrupt
  //   _T1IE = 1;//enable interrupt    
     _T1IP = 4;//set prioritet 4
     PORT_T1 = 0;
     TRIS_T1 = 0xFFFF;//port timer1 as input
-    ANSC = 0;//analog/digital pin
+    ANSCbits.ANSC14 = 0;//analog/digital pin
     PR1 = 10000;//period reset TMR1
     TMR1 = 0;//clear counter
     T1CONbits.TCS = 1;//source external clock
     T1CONbits.TCKPS = 0b00;//prescaler 1:1
     T1CONbits.TGATE = 0;//Gated Time Accumulation Enable bit
-    T1CONbits.TSYNC = 0;//Do not synchronize external clock input
+    T1CONbits.TSYNC = 1;//Do not synchronize external clock input
     T1CONbits.TSIDL = 0;//
     T1CONbits.TON = 0;//stop Timer1
 
@@ -27,19 +31,24 @@ void init_timer1(void)
 
 void init_timer23(void)
 {
-    PORT_T2 = 0;
-    TRIS_T2 = 0xFFFF;//port timer2 as input
+    long Period = 10000000;
+    int* pPeriod = &Period;
+    PORTFbits.RF2 = 0;
+    TRISFbits.TRISF2 = 1;//port timer2 as input
     mapping_pin_timer2RP0();//remap pin timer2 to RP0
     _T2IE = 0;//disable interrupt timer2
     _T3IE = 0;//disable interrupt timer3
+    _T3IF = 0;
 //    PR2 = 0;//
 //    PR3 = 0;
     T3CON = 0;//no use
+    PR2 = *pPeriod++;
+    PR3 = *pPeriod;
     TMR2 = 0;//clear counter T2
     TMR3 = 0;//clear counter T3
     T2CONbits.TCS = 1;//
-    T2CONbits.TCKPS1 = 1;//prescaler 
-    T2CONbits.TCKPS0 = 1;//1:256
+    T2CONbits.TCKPS1 = 0;//prescaler 
+    T2CONbits.TCKPS0 = 0;//1:256
     T2CONbits.TGATE = 0;
     T2CONbits.TSIDL = 0;
     T2CONbits.T32 = 1;
@@ -51,7 +60,7 @@ void init_timer23(void)
 void start_timer1_23()
 {
     T1CONbits.TON = 1;//
-//    T2CONbits.TON = 1;
+    T2CONbits.TON = 1;
 }
 
 
@@ -61,9 +70,9 @@ void mapping_pin_timer2RP0()
     __builtin_write_OSCCONL(OSCCON & 0xbf);//Unlock Registers use C30 built-in macro
     RPINR3 = 0x1E;//Assign T2 To Pin RP30
 //    RPINR4 = 0x25;//Assign T4 To Pin RPi37
-    RPOR10bits.RP21R = 7;//assign output MOSI
-    RPOR13bits.RP26R = 8;//assign output SCK
-    RPOR9bits.RP19R = 9;//assign output SS
+    RPOR2bits.RP5R = 7;//assign output MOSI
+    RPOR5bits.RP10R = 8;//assign output SCK
+//    RPOR9bits.RP19R = 9;//assign output SS
 /*    RPINR3bits.T2CKR0 = 1;//Assign T2 To Pin RP0
     RPINR3bits.T2CKR1 = 0;
     RPINR3bits.T2CKR2 = 0;
--- main_dro.c
+++ main_dro.c
@@ -18,7 +18,7 @@
 
               
 //);
- int CONFIG3 __attribute__((space(prog), address(0x2ABFA))) = 0xFFFF ;
+ int CONFIG3 __attribute__((space(prog), address(0x2ABFA))) = 0xFCFF ;
 //_CONFIG3(
 //    WPFP_WPFP255 &       // Write Protection Flash Page Segment Boundary (Highest Page (same as page 170))
 //    SOSCSEL_SOSC &       // Secondary Oscillator Power Mode Select (Secondary oscillator is in Default (high drive strength) Oscillator mode)
@@ -28,7 +28,7 @@
 //    WPCFG_WPCFGDIS &     // Write Protect Configuration Page Select (Last page (at the top of program memory) and Flash Configuration Words are not write-protected)
 //    WPEND_WPENDMEM       // Segment Write Protection End Page Select (Protected code segment upper boundary is at the last page of program memory; the lower boundary is the code page specified by WPFP)
 //);
- int CONFIG2 __attribute__((space(prog), address(0x2ABFC))) = 0xFAFF ;
+ int CONFIG2 __attribute__((space(prog), address(0x2ABFC))) = 0x9FF ;
 //_CONFIG2(
 //    POSCMOD_NONE &       // Primary Oscillator Select (Primary oscillator is disabled)
 //    IOL1WAY_ON &         // IOLOCK One-Way Set Enable (The IOLOCK bit (OSCCON<6>) can be set once, provided the unlock sequence has been completed. Once set, the Peripheral Pin Select registers cannot be written to a second time.)
@@ -52,17 +52,21 @@
 //    JTAGEN_OFF           // JTAG Port Enable (JTAG port is disabled)
 //);
 
-
+//#pragma config SOSCEL = IO
+ 
 void delay_ms(unsigned long int ms);
 
 int main(int argc, char** argv) 
 { 
+    CLKDIVbits.CPDIV = 0b00;
     init_timer1();
+    init_timer23();
     spi_init();
     init_port_led();
     unsigned int count = 0, flag = 0;
     start_timer1_23();
     led_on(10);
+    spi_txrx_AD5312(count);
     while(1)
     {
   /*     led_on(LED_SYNH); 
@@ -82,23 +86,23 @@ int main(int argc, char** argv)
            else flag = 0;
        }
        spi_txrx_AD5312(count);
-       
+ /*      
      if(flag_interrupt)
        {
            invers_LED_SYNH();
            flag_interrupt = 0;
        }
 */
-        if (_T1IF == 1)
+        if (_T3IF == 1)
         {
- //           _T1IF = 0;
-            if (count == 2)
+ //           _T13F = 0;
+            if (count == 1)
             {
  //               count = 0;
                 led_on(9);
  //               invers_LED_SYNH();
             }
-            else if(count == 4)
+            else if(count == 2)
             {
               led_off(9);
               count = 0;
@@ -106,7 +110,7 @@ int main(int argc, char** argv)
             }
               count++;
               TMR1 = 0;
-              _T1IF = 0;
+              _T3IF = 0;
         }
 
     }
